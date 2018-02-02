6cd6f95ddae6c71332408e50f8cc395678018ed9
Ash <ashley@juice49.com>
2018-02-02 22:13:32 +0000

    Format log output (closes #11)

--- cli/log.js
+++ cli/log.js
@@ -2,6 +2,17 @@
 
 const { EOL } = require('os')
 const fun = require('funstream')
+const formatDate = require('date-fns/format')
+const chalk = require('chalk')
+const prettyMs = require('pretty-ms')
+const { START_TIMER, STOP_TIMER, ADJUST_TIMER } = require('../actions')
+const { parse } = require('../lib/event-key')
+
+const strings = {
+  [START_TIMER]: chalk.green`[start]`,
+  [STOP_TIMER]: chalk.yellow`[stop]`,
+  [ADJUST_TIMER]: chalk.magenta`[adjust]`
+}
 
 module.exports = async function log ({ getDb, cli }) {
   const { main } = await getDb()
@@ -13,9 +24,41 @@ module.exports = async function log ({ getDb, cli }) {
   })
 
   await fun(events)
-    .map(({ key, value }) => [ key, value ]
-      .filter(data => data !== '')
-      .join(': ') + EOL
-    )
+    .map(format)
     .pipe(process.stdout)
+}
+
+function format ({ key, value }) {
+  const { id, event, time } = parse(key)
+
+  const label = [
+    chalk.dim(formatDate(time, 'DD/MM/YY HH:mm:ss')),
+    strings[event],
+    chalk.bold(id)
+  ].join(' ')
+
+  return [ label, formatValue(parseValue(value)) ]
+    .filter(data => typeof data !== 'undefined')
+    .join(' ') + EOL
+}
+
+function formatValue (value) {
+  if (typeof value === 'undefined') {
+    return
+  }
+
+  return chalk.dim.italic(value.sign + value.pretty)
+}
+
+function parseValue (value) {
+  if (value.length === 0) {
+    return
+  }
+
+  return {
+    pretty: prettyMs(Math.abs(value)),
+    sign: value < 0
+      ? '-'
+      : '+'
+  }
 }
